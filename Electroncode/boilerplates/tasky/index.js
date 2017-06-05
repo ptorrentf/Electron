6f8c5287409f51641a5fbc52609d93477e09c981
Pedro Torrent F <ptorrent@jri.cl>
2017-06-05 15:12:20 -0400

    Second commit

---
+++ Electroncode/boilerplates/tasky/index.js
@@ -0,0 +1,28 @@
+const electron = require('electron');
+const { app, BrowserWindow, Tray} = electron;
+const path = require('path');
+
+let mainWindow;
+let tray;
+
+app.on('ready', () => {
+    mainWindow = new BrowserWindow({
+        height: 500,
+        width: 300,
+        frame: false,
+        resizable: false,
+        show: false
+    });
+    mainWindow.loadURL(`file://${__dirname}/src/index.html`);
+
+    const iconName = process.platform === 'win32' ? 'windows-icon.png' : 'iconTemplate.png'
+    const iconPath = path.join(__dirname, `./src/assets/${iconName}`);
+    tray = new Tray(iconPath);
+    tray.on('click', () => {
+        if (mainWindow.isVisible()){
+            mainWindow.hide();
+        } else {
+            mainWindow.show();
+        }
+    });
+});
